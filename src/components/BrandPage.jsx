0b387642669008ce4195b8bb3dc3f9367df1bc9b
Soumya Moulik <156658951+dev4jlsmiths@users.noreply.github.com>
2024-07-31 17:49:30 +0530

    update

--- src/components/BrandPage.jsx
+++ src/components/BrandPage.jsx
@@ -30,31 +30,9 @@ function BrandPage() {
   }, []);
 
   return (
-    <div className="min-h-screen min-w-screen text-xs pl-8 pt-4">
-      <header className="flex justify-between items-center mb-8">
-        <div className="flex space-x-4">
-          <div className="relative">
-            <input
-              type="text"
-              placeholder="Search"
-              className="pl-10 pr-4 py-2 rounded-full border border-gray-300"
-            />
-            <svg
-              className="w-6 h-6 absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-4.35-4.35m1.4-5.65a7 7 0 11-14 0 7 7 0 0114 0z"
-              ></path>
-            </svg>
-          </div>
-        </div>
+    <div className="min-h-screen min-w-screen text-xs pl-8 pt-2">
+      <header className="flex justify-between items-center mb-4">
+        <div className="flex space-x-4"></div>
         <div className="flex items-center space-x-4">
           <div className="relative">
             <button className="flex items-center space-x-2">

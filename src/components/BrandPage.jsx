721e0c93a08eb1d08efbd78bdc6fe8884b41951c
Soumya Moulik <156658951+dev4jlsmiths@users.noreply.github.com>
2024-08-01 12:18:50 +0530

    update

--- src/components/BrandPage.jsx
+++ src/components/BrandPage.jsx
@@ -6,6 +6,7 @@ import Card from "./BrandCard";
 function BrandPage() {
   const [brands, setBrands] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     const fetchBrands = async () => {
@@ -28,11 +29,53 @@ function BrandPage() {
     fetchBrands();
   }, []);
 
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredBrands = brands.filter((brand) =>
+    brand.brand_name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div className="min-h-screen bg-gray-200 min-w-screen text-xs">
-      <header className="flex bg-white justify-end border-b pb-1 items-center">
+      <header className="flex bg-white justify-between border-b pb-1 items-center px-4">
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchQuery}
+            onChange={handleSearchChange}
+            className="pl-4 pr-4 py-1 w-64 bg-gray-200 border rounded-md"
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="absolute right-2 top-1 w-4 h-4 text-gray-500"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 21l-4.35-4.35M15.75 10.5a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0z"
+            />
+          </svg>
+        </div>
         <button className="flex items-center gap-1 px-2 py-0.5 transition-colors duration-300">
-          Settings
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="size-6 text-gray-500 pr-2 border-r-2"
+          >
+            <path
+              fillRule="evenodd"
+              d="M6.455 1.45A.5.5 0 0 1 6.952 1h2.096a.5.5 0 0 1 .497.45l.186 1.858a4.996 4.996 0 0 1 1.466.848l1.703-.769a.5.5 0 0 1 .639.206l1.047 1.814a.5.5 0 0 1-.14.656l-1.517 1.09a5.026 5.026 0 0 1 0 1.694l1.516 1.09a.5.5 0 0 1 .141.656l-1.047 1.814a.5.5 0 0 1-.639.206l-1.703-.768c-.433.36-.928.649-1.466.847l-.186 1.858a.5.5 0 0 1-.497.45H6.952a.5.5 0 0 1-.497-.45l-.186-1.858a4.993 4.993 0 0 1-1.466-.848l-1.703.769a.5.5 0 0 1-.639-.206l-1.047-1.814a.5.5 0 0 1 .14-.656l1.517-1.09a5.033 5.033 0 0 1 0-1.694l-1.516-1.09a.5.5 0 0 1-.141-.656L2.46 3.593a.5.5 0 0 1 .639-.206l1.703.769c.433-.36.928-.65 1.466-.848l.186-1.858Zm-.177 7.567-.022-.037a2 2 0 0 1 3.466-1.997l.022.037a2 2 0 0 1-3.466 1.997Z"
+              clipRule="evenodd"
+            />
+          </svg>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -61,7 +104,7 @@ function BrandPage() {
         </header>
         <main>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-6">
-            {brands.map((brand) => (
+            {filteredBrands.map((brand) => (
               <Card
                 key={brand._id}
                 brand={{
